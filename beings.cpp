9bb306ccd7cbbedca819c93faf37bdd34d4a26b0
teri934 <kulichova.t@gmail.com>
2022-02-26 11:38:55 +0100

    add and delete files

---
+++ beings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "beings.h"
+
+
+void Sheep::Simulate() {
+
+}
+
+void VioletFlower::Simulate() {
+
+}
+
+void RedFlower::Simulate() {
+
+}
+
+void Volcano::Simulate() {
+
+}
